46ef05d80a947f345ca9d108b1fdb4202698487e
Franz Lopez <franzelunico@gmail.com>
2018-02-01 15:51:50 -0400

    FLC L2T1 probando git y github

---
+++ lesson2/task2/undefined_variable.py
@@ -0,0 +1,9 @@
+"""
+Los nombres de las variables solo pueden contener letras,
+dígitos  o el carácter de subrayado, y no pueden comenzar con un dígito.
+
+Compruebe lo que sucede si utiliza una variable que aún no está definida.
+Intenta imprimir un nombre indefinido.
+"""
+variable = 1
+print(variable)
