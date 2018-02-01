400a0b0e5c6cccc0c00042d013822e87471beb8f
Franz Lopez <franzelunico@gmail.com>
2018-02-01 16:18:16 -0400

    FLC L2T4 probando git y github

---
+++ lesson2/task4/type_cast.py
@@ -0,0 +1,15 @@
+"""
+Hay varias funciones integradas que le permiten convertir un tipo de datos a otro.
+Estas funciones devuelven un nuevo objeto que representa el valor convertido.
+int(x) convierte x en un entero.
+float(x) convierte x en un número de coma flotante.
+str (x) convierte el objeto x en una representación de cadena.
+
+Convierte float_number en entero.
+"""
+number = 9
+print(type(number))   # print type of variable "number"
+
+float_number = 9.0
+print(float_number)
+print(int(float_number))
