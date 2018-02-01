68431256baf00c99f7487fcd93e2dee243e14947
Franz Lopez <franzelunico@gmail.com>
2018-02-01 15:43:10 -0400

    FLC L2T1 probando git y github

---
+++ lesson2/task1/variable_definition.py
@@ -0,0 +1,12 @@
+"""
+Los comentarios en Python comienzan con el carácter (#) e incluyen toda la línea.
+Agregue un nuevo comentario al archivo.
+"""
+a = b = 2  # Esto se llama una "asignación encadenada". Asigna el valor 2 a las variables "a" y "b".
+print("a = " + str(a))   # Explicaremos la expresión str(a) más adelante en el curso. Por ahora, se usa para convertir la variable "a" en una cadena.
+print("b = " + str(b))
+
+saludos = "saludos" # greetings
+print("saludos = " + str(saludos))
+saludos = 3
+print("saludos = " + str(saludos))
